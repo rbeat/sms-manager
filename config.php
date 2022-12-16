cda8fd4670e58c0af1fc14f3ee0ba4b972dc20ec
Rudy Grinberg <admin@rbeat.gq>
2022-12-16 21:42:18 +0200

    Initial code commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+   /**
+   * SMS Manager - Online portal to check received SMS messages from Twilio
+   * 
+   * @author Rudy G. (a.k.a. R.Beat)
+   * @copyright 2022 R.Beat (https://rbeat.gq)
+   */
+
+return [
+        // Credentials to the DB
+        'DB_log'         => 'DB_LOGIN',
+        'DB_pass'        => 'DB_PASS',
+        'DB_host'        => '127.0.0.1',         
+        'DB_name'        => 'DB_NAME', 
+
+];
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+   /**
+   * SMS Manager - Online portal to check received SMS messages from Twilio
+   * 
+   * @author Rudy G. (a.k.a. R.Beat)
+   * @copyright 2022 R.Beat (https://rbeat.gq)
+   */
+   date_default_timezone_set("UTC");
+   ini_set('display_errors', 1);
+   ini_set('display_startup_errors', 1);
+?>
